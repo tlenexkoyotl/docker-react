6fef1185faa2e5d48ef763a94144129887f5e1d0
tlenexkoyotl <tlenexkoyotl@gmail.com>
2020-09-02 23:10:38 -0500

    New App text

--- src/App.js
+++ src/App.js
@@ -9,6 +9,8 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
+
+          <h1>This is a change from the feature branch</h1>
         </p>
         <a
           className="App-link"
